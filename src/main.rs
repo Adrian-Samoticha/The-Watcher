b5bd8309c0db9638284a432014fc7be498f4351a
Adrian Samoticha <adrian@samoticha.de>
2021-12-07 19:38:22 +0100

    feat: call fs_watcher::watch in main()

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
+mod fs_watcher;
+
+use std::process::Command;
+use std::time::Duration;
+
 fn main() {
-    println!("Hello, world!");
+    let mut command = Command::new("ls");
+    
+    let result = fs_watcher::watch("./dir", Duration::from_millis(150), |event| {
+        println!("{:?}", event);
+        
+        let result = command.spawn();
+        if result.is_err() {
+            println!("Error: {:?}", result.err());
+        }
+    }, |error| {
+        eprintln!("Watch error: {:?}", error);
+    });
+    
+    if result.is_err() {
+        eprintln!("Error: {:?}", result.err().unwrap().to_string());
+    }
 }
