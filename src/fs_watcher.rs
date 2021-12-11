28a203ec1769a798a4c295c121eb6c8d100dd6d9
Adrian Samoticha <adrian@samoticha.de>
2021-12-11 11:24:10 +0100

    change: implement WatchArgs

--- src/fs_watcher.rs
+++ src/fs_watcher.rs
@@ -4,15 +4,65 @@ use notify::watcher;
 use std::time::Duration;
 use std::sync::mpsc::channel;
 
-pub fn watch<F, G>(path: &str, delay: Duration, mut callback: F, on_watch_error: G) -> std::result::Result<(), notify::Error> where
-	F: FnMut(notify::DebouncedEvent),
-	G: Fn(std::sync::mpsc::RecvError) {
+pub struct WatchArgs<'a> {
+	path: &'a str,
+	delay: Duration,
+	callback: Box<dyn FnMut(notify::DebouncedEvent)>,
+	on_watch_error: Box<dyn Fn(std::sync::mpsc::RecvError)>,
+}
+
+impl WatchArgs<'_> {
+	pub fn default() -> Self {
+		WatchArgs {
+			path: ".",
+			delay: Duration::from_millis(100),
+			callback: Box::new(|_| {}),
+			on_watch_error: Box::new(|_| {}),
+		}
+	}
 	
+	pub fn new(path: &str, delay: Duration, callback: Box<dyn FnMut(notify::DebouncedEvent)>, on_watch_error: Box<dyn Fn(std::sync::mpsc::RecvError)>) -> WatchArgs<'_> {
+		WatchArgs {
+			path,
+			delay,
+			callback,
+			on_watch_error,
+		}
+	}
+	
+	pub fn with_path(self, path: &str) -> WatchArgs {
+		WatchArgs {
+			path,
+			delay: self.delay,
+			callback: self.callback,
+			on_watch_error: self.on_watch_error,
+		}
+	}
+	
+	pub fn with_delay(mut self, delay: Duration) -> Self {
+		self.delay = delay;
+		self
+	}
+	
+	pub fn with_callback<F>(mut self, callback: F) -> Self
+	where F: FnMut(notify::DebouncedEvent) + 'static {
+		self.callback = Box::new(callback);
+		self
+	}
+	
+	pub fn with_on_watch_error<F>(mut self, on_watch_error: F) -> Self
+	where F: Fn(std::sync::mpsc::RecvError) + 'static {
+		self.on_watch_error = Box::new(on_watch_error);
+		self
+	}
+}
+
+pub fn watch(mut args: WatchArgs) -> std::result::Result<(), notify::Error> {
 	use notify::{RecursiveMode, Watcher};
 
 	let (tx, rx) = channel();
-	let mut watcher = watcher(tx, delay).unwrap();
-	let result = watcher.watch(path, RecursiveMode::Recursive);
+	let mut watcher = watcher(tx, args.delay).unwrap();
+	let result = watcher.watch(args.path, RecursiveMode::Recursive);
 	
 	if result.is_err() {
 		return result;
@@ -20,8 +70,8 @@ pub fn watch<F, G>(path: &str, delay: Duration, mut callback: F, on_watch_error:
 
 	loop {
 		match rx.recv() {
-			Ok(event) => callback(event),
-			Err(e) => on_watch_error(e),
-		}
+			Ok(event) => &(args.callback)(event),
+			Err(e) => &(args.on_watch_error)(e),
+		};
 	}
 }
--- src/main.rs
+++ src/main.rs
@@ -6,18 +6,22 @@ use std::time::Duration;
 fn main() {
     let mut command = Command::new("ls");
     
-    let result = fs_watcher::watch("./dir", Duration::from_millis(150), |event| {
-        println!("{:?}", event);
-        
-        let result = command.spawn();
-        if result.is_err() {
-            println!("Error: {:?}", result.err());
-        }
-    }, |error| {
-        eprintln!("Watch error: {:?}", error);
-    });
+    let watch_args = fs_watcher::WatchArgs::default()
+        .with_path("./dir")
+        .with_delay(Duration::from_millis(150))
+        .with_callback(move |event| {
+            println!("{:?}", event);
+            
+            let result = command.spawn();
+            if result.is_err() {
+                println!("Error: {:?}", result.err());
+            }
+        }).with_on_watch_error(|error| {
+            eprintln!("Watch error: {:?}", error);
+        });
     
-    if result.is_err() {
-        eprintln!("Error: {:?}", result.err().unwrap().to_string());
+    let watch_result = fs_watcher::watch(watch_args);
+    if watch_result.is_err() {
+        eprintln!("Error: {:?}", watch_result.err().unwrap().to_string());
     }
 }
