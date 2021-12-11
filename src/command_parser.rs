e2f4bf7fabf038b057e661f37dd4da41cb380652
Adrian Samoticha <adrian@samoticha.de>
2021-12-11 18:03:17 +0100

    fix: print error message when no command is given

--- src/command_parser.rs
+++ src/command_parser.rs
@@ -1,8 +1,15 @@
 use std::process::Command;
+use std::process;
 
 /// Receives a command as a string and parses it into a Command struct.
 pub fn parse_command(command: &str) -> Command {
     let mut command_vec: Vec<&str> = command.split_whitespace().collect();
+    
+    if command_vec.is_empty() {
+        eprintln!("Error: No command given.");
+        process::exit(1);
+    }
+    
     let mut command_struct = Command::new(command_vec.remove(0));
     for arg in command_vec {
         command_struct.arg(arg);
